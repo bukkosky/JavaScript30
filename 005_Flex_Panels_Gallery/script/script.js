c2842b9d7d2bcc40451f1a96651411dc8b2351b3
bukkosky <bukkosky@gmail.com>
2018-05-12 19:25:17 +0200

    Ajax exercise

--- 005_Flex_Panels_Gallery/script/script.js
+++ 005_Flex_Panels_Gallery/script/script.js
@@ -2,13 +2,13 @@
 
  function toggleOpen() {
      console.log('Hello')
-     this.classList.add('open');
+     this.classList.toggle('open');
  }
 
  function toggleActive(e){
      console.log(e.propertyName)
      if (e.propertyName.includes('flex')) {
-        this.classList.add('open-active');
+        this.classList.toggle('open-active');
       }
  }
  panels.forEach(panel => panel.addEventListener('click', toggleOpen));
